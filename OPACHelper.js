914acc219229397fd713e1f9526e532879f2627e
Anapopo <favoursmoe@gmail.com>
2018-07-31 02:12:36 +0800

    Update: v0.1.0 使用 ES7  语法进行重构

--- OPACHelper.js
+++ OPACHelper.js
@@ -1,142 +1,156 @@
-const config = require('./config.json')
-const URL = config.url
 const fetch = require('node-fetch')
 const fs = require('fs')
 const recognize = require('tesseractocr')
 const qs = require('querystring')
 
-async function login() {
-  return await fetch(`http://${URL}/reader/login.php`)
-    .then(res=>{
-      const cookie = res.headers.get('Set-Cookie')
-      return cookie.match(/(.*); path=\/; HttpOnly/)[1]
-    })
-    .then(cookie=>saveCookie(cookie))
+class OPACHelper {
+    constructor(options = {}) {
+        this.options = options;
+        this.needRelogin = false;
+    }
+    // return verified cookie
+    async login() {
+        if (config.cookie != "" && this.needRelogin == false) {
+            console.log(`[*] 检测到 Cookie: [${config.cookie}]`)
+            console.log('[*] 正在使用此 Cookie 进行登录...')
+            return Promise.resolve(config.cookie)
+        }
+        // get cookie for the next step
+        const res = await fetch(`http://${config.url}/reader/login.php`)
+        var cookie = res.headers.get('Set-Cookie')
+        cookie = cookie.match(/(.*); path=\/; HttpOnly/)[1]
+        console.log(`[1/3] 已拉取到新 Cookie: [${cookie}]`)
+        const headers1 = {
+            'Referer': `http://${config.url}/reader/login.php`,
+            'Cookie': cookie
+        }
+        // download captcha gif
+        await fetch(`http://${config.url}/reader/captcha.php`, { headers:headers1 })
+            .then(res => {
+                const dest = fs.createWriteStream('/tmp/captcha.gif')
+                res.body.pipe(dest)
+                return dest
+            })
+
+        let code = await recognize('/tmp/captcha.gif', {})
+        code = code.trim()
+        console.log(`[2/3] 验证码识别成功 ${code}`)
+        
+        const headers2 = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': `http://${config.url}/reader/redr_verify.php`,
+            'Cookie': cookie
+        }
+        const param = `number=${config.user}&passwd=${config.passwd}&captcha=${code}&select=cert_no&returnUrl=`
+        // console.log(param)
+        const ok = await fetch(`http://${config.url}/reader/redr_verify.php`, {
+            method: 'POST',
+            headers: headers2,
+            body: param
+        })
+        .then(res=>res.ok)
+    
+        if (ok) {
+            console.log(`[3/3] Cookie [${cookie}] 自动认证通过!`)
+            return Promise.resolve(cookie)
+        } else {
+            throw new Error('verify-fail')
+        }
+    }
+    async loadConfig() {
+        global.config = require('./config.json')
+        if (config.url == "" || config.user == "" || config.passwd == "")
+            throw new Error('empty-config')
+    }
+    async saveCookie(cookie) {
+        if (config.cookie == cookie) 
+            return Promise.resolve();
+        config.cookie = cookie
+        fs.writeFile('./config.json', JSON.stringify(config, null, 2), err=>{
+            if (err) throw err
+            this.needRelogin = false;
+            console.log(`[*] Cookie [${cookie}] 已保存至配置文件.`)
+            return Promise.resolve()
+        })
+    }
+    /**
+     * 获取读者当前借阅书籍的简略信息
+     * @return {String}
+     */
+    async getLiteBooks() {
+        // console.log("getLiteBook时的Cookie", config.cookie)
+        if (config.cookie == "") {
+            console.log('[!] Cookie不存在.')
+            throw new Error('nonexist-cookie')
+        }
+
+        const body = await fetch(
+            `http://${config.url}/reader/book_lst.php`,
+            { headers: { 'Cookie': config.cookie } }
+        ).then(res=>res.text())
+
+        // test whether need relogin
+        const needLogin = body.match(/<input.*value="登录">/)
+        if(!needLogin) {
+            const base64 = body.match(/<img src=".*qrcode=(.*?)" border="0" \/>/)[1]
+            const res = Buffer.from(qs.unescape(base64), 'base64').toString()
+            // console.log(res)
+            return this.handleBooks2Json(res)
+        } else {
+            console.log('[!] Cookie过期啦, 正在尝试续Cookie...')
+            this.needRelogin = true;
+            throw new Error('cookie-expire')
+        }
+    }
+    /**
+     * 将获取到的借阅信息转JSON
+     * @param {String} text
+     * @return {JSON}
+     */
+    handleBooks2Json(text) {
+        let json = []
+        const pattern = /(.*)\u{20}{8}\r\n/ug
+        //console.log(new Date().toLocaleDateString('zh'))
+        text.match(pattern).forEach(val=>{
+          let [name, time] = val.trim().split(/\u{20}/u)
+          let mils = new Date(time).getTime() - new Date().getTime()
+          let days = `${Math.floor(mils / (24*3600*1000))}d`
+          //console.log(days)
+          json.push({ name, time, days})
+        })
+        return JSON.stringify(json, null, 2)
+    }
+    /**
+     * 测试获取个人主页
+     * @return {void}
+     */
+    async getHomePage() {
+        const headers = {
+            'Cookie': config.cookie
+        }
+        return await fetch(`http://${URL}/reader/redr_info.php`, {headers})
+        .then(res => {
+            if (res.ok)
+            return res.text()
+        })
+    }
+    async run() {
+        try {
+            await this.loadConfig()
+            const cookie = await this.login()
+            await this.saveCookie(cookie)
+            let res = await this.getLiteBooks()
+            console.log(res)
+        } catch (err) {
+            if (err.message == 'empty-config') {
+                console.log('[!] 请完整填写 config.json 文件');
+            } else if (err.message == 'cookie-expire') {
+                return this.run();
+            } else {
+                console.log(err)
+            }
+        }
+    }
 }
 
-function saveCookie(cookie) {
-  config.cookie = cookie
-  fs.writeFileSync('./config.json', JSON.stringify(config, null, 2))
-}
-
-async function getCode() {
-  const headers = {
-    'Referer': `http://${URL}/reader/login.php`,
-    'Cookie': config.cookie
-  }
-  return await fetch(`http://${URL}/reader/captcha.php`, {headers})
-    .then(res=>{
-      const dest = fs.createWriteStream('./captcha.gif')
-      res.body.pipe(dest)
-      return dest
-    })
-}
-
-async function doVerify() {
-  let code = await recognize('captcha.gif', {})
-
-  const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Referer': `http://${URL}/reader/redr_verify.php`,
-    'Cookie': config.cookie
-  }
-  const param = `number=${config.user}&passwd=${config.passwd}&captcha=${code.trim()}&select=cert_no&returnUrl=`
-  // console.log(param)
-  return await fetch(`http://${URL}/reader/redr_verify.php`, {
-    method: 'POST',
-    headers,
-    body: param
-  })
-  .then(res=>{
-    if (res.ok)
-      console.log('[*] 通过自动认证!')
-  })
-}
-
-/**
- * 测试获取个人主页
- * @return {void}
- */
-async function getHomePage() {
-  const headers = {
-    'Cookie': config.cookie
-  }
-  return await fetch(`http://${URL}/reader/redr_info.php`, {headers})
-    .then(res => {
-      if (res.ok)
-        return res.text()
-    })
-}
-
-/**
- * 获取读者当前借阅书籍的简略信息
- * @return {String}
- */
-async function getLiteBooks() {
-  if (config.cookie == "") {
-    console.log('[!] Cookie不存在，正在尝试自动登录...')
-    await doFirstLogin()
-  }
-
-  const headers = {
-    'Cookie': config.cookie
-  }
-  return await fetch(`http://${URL}/reader/book_lst.php`, {headers})
-    .then(res=>res.text())
-    .then(async body=>{
-      const needLogin = body.match(/<input.*value="登录">/)
-      if(!needLogin) {
-        const base64 = body.match(/<img src=".*qrcode=(.*?)" border="0" \/>/)[1]
-        const res = Buffer.from(qs.unescape(base64), 'base64').toString()
-        return res
-      } else {
-        console.log('[!] Cookie过期啦, 正在尝试续Cookie...')
-        await doFirstLogin()
-        return await getLiteBooks()
-      }
-
-    })
-}
-
-/**
- * 将获取到的借阅信息转JSON
- * @param {String} text
- * @return {JSON}
- */
-function handleBooks2Json(text) {
-  let json = []
-  const pattern = /(.*)\u{20}{8}\r\n/ug
-  //console.log(new Date().toLocaleDateString('zh'))
-  text.match(pattern).forEach(val=>{
-    let [name, time] = val.trim().split(/\u{20}/u)
-    let mils = new Date(time).getTime() - new Date().getTime()
-    let days = `${Math.floor(mils / (24*3600*1000))}d`
-    //console.log(days)
-    json.push({ name, time, days})
-  })
-  console.log(JSON.stringify(json, null, 2))
-  return JSON.stringify(json, null, 2)
-
-}
-
-/**
- * 第一次登录或者Cookie过期时执行
- * @return {void}
- */
-async function doFirstLogin() {
-  await login()
-  await getCode()
-  await doVerify()
-}
-
-(async function () {
-  //await getHomePage()
-  if (config.url == "" || config.user == "" || config.passwd == "") {
-    console.error("[!] 请完整填写config.json.")
-    return 
-  }
-  let res = await getLiteBooks().catch(err=>console.error(err))
-  handleBooks2Json(res)
-  //console.log(res)
-})()
-
+module.exports = OPACHelper;
---
+++ index.js
@@ -0,0 +1,5 @@
+const OPACHelper = require('./OPACHelper')
+
+const app = new OPACHelper()
+
+app.run()
